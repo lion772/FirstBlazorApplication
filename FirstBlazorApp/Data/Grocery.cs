ca6096cd7cda149c10deb5c06a3103cf7ca5e114
william <william.steinkedemello@gmail.com>
2022-07-27 11:08:01 +0200

    fetching weather forecast API

--- FirstBlazorApp/Data/Grocery.cs
+++ FirstBlazorApp/Data/Grocery.cs
@@ -1,9 +1,17 @@
-﻿namespace FirstBlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstBlazorApp.Data
+
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should contain less than 15 characters.")]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1, 10000, ErrorMessage = "Valid Price Range is between 1 and 10000.")]
         public float Price { get; set; }
-        
+
     }
 }
